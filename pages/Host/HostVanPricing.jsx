2122ca3c511758586f634226cbfe7116b9a052a0
sunnySavlot <sunny.savlot@gmail.com>
2023-10-25 19:02:13 +0530

    clearing up

--- pages/Host/HostVanPricing.jsx
+++ pages/Host/HostVanPricing.jsx
@@ -2,7 +2,6 @@ import { useOutletContext } from "react-router-dom"
 
 export default function HostVanPricing() {
   const [hostedVanDetails, setHostVanDetails] = useOutletContext()
-  console.log(hostedVanDetails)
   return (
     <>
       <p className="host-van-pricing"><span className="host-van-pricing-amount">${hostedVanDetails.price.toFixed(2)}</span>/day</p>
