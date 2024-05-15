f141e62a2ba657e46f581db3cfca2e548c8c0911
sunnySavlot <sunny.savlot@gmail.com>
2024-05-15 21:00:12 +0530

    Added relative path to back to vans and prettified

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,26 +1,32 @@
-import { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
-import VanTypeBadge from "../../components/VanTypeBadge"
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import VanTypeBadge from "../../components/VanTypeBadge";
 
 export default function VanDetail() {
-  const params = useParams()
-  const [van, setVan] = useState({})
+  const params = useParams();
+  const [van, setVan] = useState({});
   useEffect(() => {
     fetch(`/api/vans/${params.id}`)
-      .then(res => res.json())
-      .then(data => setVan(data.vans))
-  }, [params])
-  return(
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params]);
+  return (
     <div className="van-detail-container">
-      <Link className="back-to-all-vans-link" to='/vans'>
-        <div><span>← </span><span className="back-to-home">Back to all vans</span></div>
+      <Link className="back-to-all-vans-link" to=".." relative="path">
+        <div>
+          <span>← </span>
+          <span className="back-to-home">Back to all vans</span>
+        </div>
       </Link>
-      <img className="van-img" src={van.imageUrl} alt={van.name}/>
-      <VanTypeBadge type={van.type}/>
+      <img className="van-img" src={van.imageUrl} alt={van.name} />
+      <VanTypeBadge type={van.type} />
       <h1>{van.name}</h1>
-      <div><span className="van-price">${van.price}</span><span>/day</span></div>
+      <div>
+        <span className="van-price">${van.price}</span>
+        <span>/day</span>
+      </div>
       <p>{van.description}</p>
       <button className="rent-van-btn">Rent this van</button>
     </div>
-  )
+  );
 }
