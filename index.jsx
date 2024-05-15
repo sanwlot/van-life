cbd77a2d965dabf5596eab623e038b80f607581e
sunnySavlot <sunny.savlot@gmail.com>
2024-05-15 20:00:50 +0530

    added relative path and link and prettified the code

--- index.jsx
+++ index.jsx
@@ -1,48 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from './pages/Vans/Vans'
-import VanDetail from './pages/Vans/VanDetail'
-import Layout from './components/Layout'
-import Dashboard from './pages/Host/Dashboard'
-import Income from './pages/Host/Income'
-import Reviews from './pages/Host/Reviews'
-import HostLayout from './components/HostLayout'
-import HostedVans from './pages/Host/HostedVans'
-import HostedVanDetails from './pages/Host/HostedVanDetails'
-import HostVanInfo from './pages/Host/HostVanInfo'
-import HostVanPhotos from './pages/Host/HostVanPhotos'
-import HostVanPricing from './pages/Host/HostVanPricing'
-import './server'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import HostLayout from "./components/HostLayout";
+import HostedVans from "./pages/Host/HostedVans";
+import HostedVanDetails from "./pages/Host/HostedVanDetails";
+import HostVanInfo from "./pages/Host/HostVanInfo";
+import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import HostVanPricing from "./pages/Host/HostVanPricing";
+import "./server";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Layout /> }>
-          <Route index element={ <Home />} />
-          <Route path="about" element={ <About />} />
-          <Route path="vans" element={ <Vans />} />
-          <Route path='vans/:id' element={ <VanDetail />}/>
-          <Route path="host" element={ <HostLayout />} > 
-            <Route index element={ <Dashboard/>} /> 
-            <Route path="income" element={ <Income/>} />
-            <Route path="vans" element={ <HostedVans/>} />
-            <Route path="vans/:id" element={ <HostedVanDetails/>}>
-              <Route index element={ <HostVanInfo />} />
-              <Route path='pricing' element={ <HostVanPricing />} />
-              <Route path='photos' element={ <HostVanPhotos />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="vans" element={<HostedVans />} />
+            <Route path="vans/:id" element={<HostedVanDetails />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
             </Route>
-            <Route path="reviews" element={ <Reviews />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
